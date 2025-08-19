8debe55b736cf9b2a64e4d4a032c4b08ec0cbaf4
damir <damir1petkovic@gmail.com>
2025-08-19 11:10:31 +0200

    revert to direct text encoding & sha256

--- assets/js/phoenix_live_view/my_util.js
+++ assets/js/phoenix_live_view/my_util.js
@@ -1,9 +1,8 @@
 import sha256 from "js-sha256"
 
-export function sha256JSON(obj){
-  const str = JSON.stringify( obj);
-  const bytes = new TextEncoder().encode( str)  // Uint8Array of UTF-8
-  return sha256(bytes)  // js-sha256 accepts Uint8Array input
+export function sha256JSON( obj){
+  const bytes = new TextEncoder().encode( obj)  // Uint8Array of UTF-8
+  return sha256( bytes)  // js-sha256 accepts Uint8Array input
 }// util.js
 
 export function formatDate(date) {
