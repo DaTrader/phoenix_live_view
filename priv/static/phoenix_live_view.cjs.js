c857bdcf865c8e790bed5d251eb8f9ff96845bc4
damir <damir1petkovic@gmail.com>
2025-08-19 09:44:48 +0200

    stringify before encoding to bytes

--- priv/static/phoenix_live_view.cjs.js
+++ priv/static/phoenix_live_view.cjs.js
@@ -1861,7 +1861,8 @@ var hooks_default = Hooks;
 // js/phoenix_live_view/my_util.js
 var import_js_sha256 = __toESM(require_sha256());
 function sha256JSON(obj) {
-  const bytes = new TextEncoder().encode(obj);
+  const str = JSON.stringify(obj);
+  const bytes = new TextEncoder().encode(str);
   return (0, import_js_sha256.default)(bytes);
 }
 
--- priv/static/phoenix_live_view.js
+++ priv/static/phoenix_live_view.js
@@ -1878,7 +1878,8 @@ removing illegal node: "${(childNode.outerHTML || childNode.nodeValue).trim()}"
   // js/phoenix_live_view/my_util.js
   var import_js_sha256 = __toESM(require_sha256());
   function sha256JSON(obj) {
-    const bytes = new TextEncoder().encode(obj);
+    const str = JSON.stringify(obj);
+    const bytes = new TextEncoder().encode(str);
     return (0, import_js_sha256.default)(bytes);
   }
 
